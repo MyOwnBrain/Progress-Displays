69007deb2c732bb5d7bcb107fd04bff3511c26f5
MyOwnBrain <81764985+MyOwnBrain@users.noreply.github.com>
2022-12-31 22:10:24 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+/** @type {SVGCircleElement} */
+
+const circle = document.getElementById("progress_circle");
+const bar_label = document.getElementById("bar_label");
+const percent_texts = document.querySelectorAll(".percent_text")
+let radius = circle.r.baseVal.value;
+
+let u = radius * 2 * Math.PI;
+
+circle.style.strokeDasharray = u.toString();
+circle.style.strokeDashoffset = u.toString();
+
+function progress(percent) {
+    if (percent > 100 || percent < 0 ||  typeof percent !== "number") {
+        console.error("Sytax error:\n[line 12]: progress(percent)\npercent must be an number in the range from 0 to 100")
+        return
+    }
+
+    circle.style.strokeDashoffset = u - (u / 100) * percent;
+    bar_label.style.transform = `translateX(-${100 - percent}%)`;
+
+    let curr_percent_text = (percent.toString().split(".")[1] !== undefined) ? parseFloat("0." + percent.toFixed(1).split(".")[1]) : 0;
+    let speed = 1000 / percent;
+    
+    let count = setInterval(() => {
+        percent_texts.forEach((element) => {
+            element.innerText = curr_percent_text + "%";
+        })
+        if (Math.round(curr_percent_text) === Math.round(percent)) clearInterval(count);
+        curr_percent_text++;
+    }, speed)
+}
+
+progress(Math.random() * 100)
